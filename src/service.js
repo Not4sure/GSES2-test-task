9a05381148fe88aa0acacff1041bb78258e5c1bf
NotSure <juliy.maj@gmail.com>
2022-07-31 10:26:14 +0300

    fixed /subscribe endpoint

--- src/service.js
+++ src/service.js
@@ -2,16 +2,30 @@ import fetch from "node-fetch";
 import * as constants from "constants";
 import fs from 'fs'
 import * as util from "util";
+import e from "express";
 
 const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
 const writeFile = (fileName, data) => util.promisify(fs.writeFile)(fileName, data, 'utf8');
-
-const dataPath = process.env.DATA_PATH ?? '../data'
 
 const origin = 'api.coingecko.com'
 const path = '/api/v3/simple/price'
 const crypto = 'bitcoin'
 const currency = 'uah'
+
+const dataPath = process.env.DATA_PATH ?? './data'
+
+fs.access(dataPath, (err) => {
+    if (err) {
+        fs.mkdir(dataPath, (err) => {
+            if (err) {
+                console.log(error)
+                process.exit(1)
+            } else {
+                console.log("Data directory created")
+            }
+        });
+    }
+});
 
 class Service {
 
@@ -24,8 +38,14 @@ class Service {
 
 
     async saveEmail(email) {
-        const file = `${dataPath}/mails.json`
-        const emails = JSON.parse(await readFile(file))
+        const file = `${dataPath}/emails.json`
+        let  emails = []
+
+        try {
+             emails = JSON.parse(await readFile(file))
+        } catch (e) {
+            console.log('File don`t exist')
+        }
 
         if (!emails.includes(email)) {
             emails.push(email)
@@ -34,6 +54,8 @@ class Service {
         }
         return false
     }
+
+
 
 
 
